05cd65f5d92d6abcc261f52285d46fb00a0eaf79
natewu <36091727+natewu@users.noreply.github.com>
2024-07-02 23:24:56 -0600

    websocket

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,28 @@ import './App.css';
 
 import { Canvas } from "@react-three/fiber";
 import MovingBox from "./Box";
+import useWebSocket from 'react-use-websocket';
+
+const WS_URL = "ws://localhost:8765/clinic"
 
 function App() {
+  // Listen to the websocket and print to console using react-use-websocket
+
+  const { sendMessage, lastMessage, readyState } = useWebSocket(WS_URL, {
+    onOpen: () => {
+      console.log('opened');
+    },
+    onClose: () => {
+      console.log('closed');
+    },
+    onMessage: (message) => {
+      console.log(message.data);
+    },
+    onError: (error) => {
+      console.log('error', error);
+    },
+  });
+
   return (
     <div className="App">
       <Canvas>
