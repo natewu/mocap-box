babee1535309affa2241207002793d96ceda8033
natewu <36091727+natewu@users.noreply.github.com>
2024-07-09 01:27:31 -0600

    calculation engine

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import './App.css';
 
 import { Canvas } from "@react-three/fiber";
 import MovingBox from "./Box";
+import { useState } from "react";
 import useWebSocket from 'react-use-websocket';
 
 const WS_URL = "ws://localhost:8765/clinic"
 
 function App() {
-  // Listen to the websocket and print to console using react-use-websocket
-
+  const [data, setData] = useState<DataVector>();
   const { sendMessage, lastMessage, readyState } = useWebSocket(WS_URL, {
     onOpen: () => {
       console.log('opened');
@@ -17,7 +17,12 @@ function App() {
       console.log('closed');
     },
     onMessage: (message) => {
-      console.log(message.data);
+      // Convert json into a DataVector object
+      const data = JSON.parse(message.data);
+      
+      const dataVector = new DataVector(data[Object.keys(data)[0]].mac, data[Object.keys(data)[0]].t, data[Object.keys(data)[0]].ax, data[Object.keys(data)[0]].ay, data[Object.keys(data)[0]].az, data[Object.keys(data)[0]].gx, data[Object.keys(data)[0]].gy, data[Object.keys(data)[0]].gz);
+      
+      setData(dataVector);
     },
     onError: (error) => {
       console.log('error', error);
@@ -29,10 +34,32 @@ function App() {
       <Canvas>
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
-        <MovingBox />
+        <MovingBox data={data}/>
       </Canvas>
     </div>
   );
+}
+
+export class DataVector{
+  public mac: string;
+  public t: number;
+  public ax: number;
+  public ay: number;
+  public az: number;
+  public gx: number;
+  public gy: number;
+  public gz: number;
+
+  constructor(mac: string, t: number, ax: number, ay: number, az: number, gx: number, gy: number, gz: number){
+    this.mac = mac;
+    this.t = t;
+    this.ax = ax;
+    this.ay = ay;
+    this.az = az;
+    this.gx = gx;
+    this.gy = gy;
+    this.gz = gz;
+  }
 }
 
 export default App;
--- src/Box.tsx
+++ src/Box.tsx
@@ -1,11 +1,16 @@
+import { Box, Edges } from "@react-three/drei";
 import React, { useEffect, useRef, useState } from 'react';
 
+import { DataVector } from "./App";
 import { Mesh } from 'three';
 import { useFrame } from '@react-three/fiber';
 
-const MovingBox: React.FC = () => {
+function MovingBox({ data }: { data: DataVector | undefined }) {
   const boxRef = useRef<Mesh>(null);
   const [mouse, setMouse] = useState({ x: 0, y: 0 });
+
+  // Initialize the Engine with the data
+  const engineRef = useRef<Engine>(new Engine(data));
 
   const handleMouseMove = (event: MouseEvent) => {
     setMouse({
@@ -22,19 +27,66 @@ const MovingBox: React.FC = () => {
   }, []);
 
   useFrame(() => {
-    if (boxRef.current) {
-      // Rotate box based on mouse
-      boxRef.current.rotation.x += (mouse.y - boxRef.current.rotation.x) * 0.5;
-      boxRef.current.rotation.y += (mouse.x - boxRef.current.rotation.y) * 0.5;
+    if (boxRef.current && data) {
+      const currentTime = performance.now();
+
+      // Update the engine with the new data
+      engineRef.current.updateData(data);
+
+      // Calculate the new rotations
+      const { rotationX, rotationY } = engineRef.current.calculateRotation(currentTime);
+
+      // Rotate box based on the engine calculations
+      boxRef.current.rotation.x = rotationX;
+      boxRef.current.rotation.y = rotationY;
     }
   });
 
   return (
     <mesh ref={boxRef}>
-      <boxGeometry args={[2, 2, 2]} />
-      <meshStandardMaterial color="orange" />
+      <Box args={[2, 2, 2]}>
+        <Edges color="black" />
+        <meshStandardMaterial color="orange" />
+      </Box>
     </mesh>
   );
 };
+
+export class Engine {
+  public data: DataVector | undefined;
+  private rotationX: number = 0;
+  private rotationY: number = 0;
+  private lastUpdateTime: number = 0;
+
+  constructor(data: DataVector | undefined) {
+    this.data = data;
+  }
+
+  public updateData(data: DataVector | undefined) {
+    this.data = data;
+  }
+
+  public calculateRotation(currentTime: number) {
+    if (!this.data) {
+      return { rotationX: this.rotationX, rotationY: this.rotationY };
+    }
+
+    const { gx, gy } = this.data;
+
+    if (this.lastUpdateTime === 0) {
+      this.lastUpdateTime = currentTime;
+      return { rotationX: this.rotationX, rotationY: this.rotationY };
+    }
+
+    const deltaTime = (currentTime - this.lastUpdateTime) / 1000; // Convert ms to seconds
+    this.lastUpdateTime = currentTime;
+
+    // Integrate angular velocities to get the rotation
+    this.rotationX += gx * deltaTime;
+    this.rotationY += gy * deltaTime;
+
+    return { rotationX: this.rotationX, rotationY: this.rotationY };
+  }
+}
 
 export default MovingBox;
